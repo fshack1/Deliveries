85c510793e8ea8138e456c1fe92848a256af11a3
Fia Hacklin <fiahck@icloud.com>
2025-01-02 18:00:33 +0100

    Update tests

--- test/models/chat_session_test.rb
+++ test/models/chat_session_test.rb
@@ -2,16 +2,14 @@ require "test_helper"
 
 class ChatSessionTest < ActiveSupport::TestCase
   setup do
-    @chat_session = ChatSession.create!(data: [ { "role" => "user", "content" => "Test message" } ])
+    @chat_session = ChatSession.create!(
+      data: [ { "role" => "user", "content" => "Test message" } ],
+      user_id: 3
+    )
   end
 
   test "conversation data" do
     assert_equal([ { "role" => "user", "content" => "Test message" } ], @chat_session.conversation)
-  end
-
-  test "conversation returns an empty array when data is nil" do
-    @chat_session.update!(data: nil)
-    assert_equal([], @chat_session.conversation)
   end
 
   test "update_conversation! updates the data attribute" do
@@ -22,8 +20,8 @@ class ChatSessionTest < ActiveSupport::TestCase
   end
 
   test "cleanup_old_sessions destroys sessions older than 2 hours" do
-    old_session = ChatSession.create!(data: [], updated_at: 3.hours.ago)
-    recent_session = ChatSession.create!(data: [], updated_at: 1.hour.ago)
+    old_session = ChatSession.create!(data: [], updated_at: 3.hours.ago, user_id: 1)
+    recent_session = ChatSession.create!(data: [], updated_at: 1.hour.ago, user_id: 1)
 
     ChatSession.cleanup_old_sessions
 
