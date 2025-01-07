c03022c100c23a1af521c162f436ad96e4a1ac63
Fia Hacklin <fiahck@icloud.com>
2025-01-07 15:16:50 +0100

    Fix bug with time selector form by removing possibility to have seconds in db entries

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Delivery < ApplicationRecord
+  before_validation :strip_seconds_from_scheduled_time
+
   validates :pickup_address, :delivery_address, :weight, :distance, :scheduled_time, presence: true
   validates :weight, :distance, numericality: { greater_than: 0 }
   validate :scheduled_time_within_valid_range
@@ -9,6 +11,12 @@ class Delivery < ApplicationRecord
 
   private def calculate_cost
     self.cost = (distance * 2) + (weight * 1.5)
+  end
+
+  private def strip_seconds_from_scheduled_time
+    if scheduled_time.present?
+      self.scheduled_time = scheduled_time.change(sec: 0)
+    end
   end
 
   private def scheduled_time_within_valid_range
